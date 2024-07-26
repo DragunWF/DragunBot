a3441e488ca4a084b70735e53edd038b07708bb5
DragunDev <plarisanmarc@gmail.com>
2024-07-26 18:53:21 +0800

    Update utils.py Move get_bot_dir_path() method

--- bot/helpers/utils.py
+++ bot/helpers/utils.py
@@ -4,16 +4,17 @@ from pathlib import Path
 class Utils:
     @staticmethod
     def list_files(directory: str) -> list[str]:
-        def get_bot_dir_path() -> str:
-            directories: list[str] = Path(__file__).__str__().split('\\')
-            while directories[-1] != "bot":
-                directories.pop()
-            return "\\".join(directories)
-
         try:
-            path = Path(f"{get_bot_dir_path()}/{directory}")
+            path = Path(f"{Utils.get_bot_dir_path()}/{directory}")
             files = [f.name for f in path.iterdir() if f.is_file()]
             return files
         except FileNotFoundError:
             print(f"The directory {directory} does not exist.")
             return []
+
+    @staticmethod
+    def get_bot_dir_path() -> str:
+        directories: list[str] = Path(__file__).__str__().split('\\')
+        while directories[-1] != "bot":
+            directories.pop()
+        return "\\".join(directories)
