eb46d963a4ff6850d1379b99d82b0ce5289e5430
DragunDev <plarisanmarc@gmail.com>
2024-07-27 05:16:01 +0800

    Update utils.py - Fix bug with hexadecimal value colors

--- bot/helpers/utils.py
+++ bot/helpers/utils.py
@@ -4,25 +4,25 @@ from pathlib import Path
 
 class Utils:
     __colors = {
-        "red": "0xFF0000",
-        "orange": "0xFFA500",
-        "yellow": "0xFFFF00",
-        "green": "0x00FF00",
-        "blue": "0x0000FF",
-        "purple": "0x800080",
-        "pink": "0xFFC0CB",
-        "gray": "0x808080",
-        "black": "0x000000",
-        "white": "0xFFFFFF",
-        "dark red": "0x8B0000",
-        "light coral": "0xF08080",
-        "gold": "0xFFD700",
-        "lime": "0x00FF00",
-        "aqua": "0x00FFFF",
-        "royal blue": "0x4169E1",
-        "medium purple": "0x9370DB",
-        "hot pink": "0xFF69B4",
-        "silver": "0xC0C0C0"
+        "red": 0xFF0000,
+        "orange": 0xFFA500,
+        "yellow": 0xFFFF00,
+        "green": 0x00FF00,
+        "blue": 0x0000FF,
+        "purple": 0x800080,
+        "pink": 0xFFC0CB,
+        "gray": 0x808080,
+        "black": 0x000000,
+        "white": 0xFFFFFF,
+        "dark red": 0x8B0000,
+        "light coral": 0xF08080,
+        "gold": 0xFFD700,
+        "lime": 0x00FF00,
+        "aqua": 0x00FFFF,
+        "royal blue": 0x4169E1,
+        "medium purple": 0x9370DB,
+        "hot pink": 0xFF69B4,
+        "silver": 0xC0C0C0
     }
 
     @staticmethod
@@ -43,13 +43,13 @@ class Utils:
         return "\\".join(directories)
 
     @staticmethod
-    def get_color(color: str) -> str:
-        DEFAULT_COLOR = Utils.__colors["Purple"]
+    def get_color(color: str) -> int:
+        DEFAULT_COLOR = Utils.__colors["purple"]
         if color in Utils.__colors:
             return Utils.__colors[color]
         return DEFAULT_COLOR
 
     @staticmethod
-    def get_random_color() -> str:
-        selectable_colors = tuple(Utils.__colors.keys())
-        return selectable_colors[random.randint(0, len(selectable_colors)) - 1]
+    def get_random_color() -> int:
+        color_keys = tuple(Utils.__colors.keys())
+        return Utils.__colors[color_keys[random.randint(0, len(color_keys)) - 1]]
