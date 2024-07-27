9982eb054f6d027c9f605f8d02a2c1062aaf6bdc
DragunDev <plarisanmarc@gmail.com>
2024-07-28 01:11:29 +0800

    Create debug.py - Create error handlers to reduce "try catch" duplication Via utilizing decorators and in-built error handlers within the Discord module

---
+++ bot/helpers/debug.py
@@ -0,0 +1,33 @@
+import traceback
+import functools
+
+import discord
+from discord import app_commands
+
+
+class Debug:
+    @staticmethod
+    def error_handler(coro):
+        @functools.wraps(coro)
+        async def wrapper(*args, **kwargs):
+            try:
+                await coro(*args, **kwargs)
+            except Exception as err:
+                TB_STR = ''.join(traceback.format_exception(
+                    type(err),  # Type of the exception
+                    err,  # The actual exception instance
+                    err.__traceback__  # The traceback object
+                ))
+                print(f'Error in {coro.__name__}:\n{TB_STR}')
+        return wrapper
+
+    @staticmethod
+    async def on_app_command_error(interaction: discord.Interaction, err: app_commands.AppCommandError):
+        await interaction.response.send_message("An error occurred while executing the command.",
+                                                ephemeral=True)
+        TB_STR = ''.join(traceback.format_exception(
+            type(err),
+            err,
+            err.__traceback__
+        ))
+        print(f'Error in command "{interaction.command.name}": {TB_STR}')
