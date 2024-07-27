55b33838cc582d6a2ba1e9be0c364c13509d35b1
DragunDev <plarisanmarc@gmail.com>
2024-07-28 01:35:27 +0800

    Update snipe.py - Implement basic sniping

--- bot/commands/snipe.py
+++ bot/commands/snipe.py
@@ -12,22 +12,21 @@ class Snipe(commands.Cog):
 
     @discord.app_commands.command(name="snipe", description="Show the most recently deleted message")
     async def snipe(self, interaction: discord.Interaction):
-        try:
-            deleted_message: discord.Message = SessionData.get_recent_deleted_message()
-            if deleted_message is None:
-                await interaction.response.send_message("My spellbook doesn't have any deleted messages stored at the moment!")
-            elif deleted_message.author.id == ConfigManager.owner_id():
-                await interaction.response.send_message("Thou shall not snipe the overlord!")
-            else:
-                embed = discord.Embed(title="Deleted Message",
-                                      description=deleted_message.content,
-                                      color=Utils.get_color("royal blue"))
-                embed.set_author(name=deleted_message.author.name,
-                                 icon_url=deleted_message.author.avatar.url)
-                embed.set_footer(text=f"#{deleted_message.channel.name}")
-                await interaction.response.send_message(embed=embed)
-        except Exception as err:
-            print(err)
+        deleted_message: discord.Message = SessionData.get_recent_deleted_message(
+            interaction.guild_id)
+        if deleted_message is None:
+            await interaction.response.send_message("My spellbook doesn't have any deleted messages stored at the moment!")
+        elif deleted_message.author.id == ConfigManager.owner_id():
+            await interaction.response.send_message("Thou shall not snipe the overlord!")
+        else:
+            embed = discord.Embed(title="Deleted Message",
+                                  description=deleted_message.content,
+                                  color=Utils.get_color("royal blue"))
+            embed.set_author(name=deleted_message.author.name,
+                             icon_url=deleted_message.author.avatar.url)
+            embed.add_field(name=f"From #{deleted_message.channel.name}", value=deleted_message.content)
+            embed.set_footer(text=f"Guild: {deleted_message.guild.name}")
+            await interaction.response.send_message(embed=embed)
 
     @discord.app_commands.command(name="esnipe", description="Show the most recently edited message")
     async def esnipe(self, interaction: discord.Interaction):
