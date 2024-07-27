587e5fc8d5e35749e52c439b091c0730229a1f90
DragunDev <plarisanmarc@gmail.com>
2024-07-28 01:32:10 +0800

    Update session_data.py - Fix bug with attributes

--- bot/helpers/session_data.py
+++ bot/helpers/session_data.py
@@ -7,26 +7,26 @@ class SessionData:
     __edited_messages: dict[int, LifoQueue] = {}
 
     @staticmethod
-    def get_recent_deleted_message() -> discord.Message | None:
-        if not SessionData.__deleted_messages.empty():
-            return SessionData.__deleted_messages.get()
+    def get_recent_deleted_message(guild_id: int) -> discord.Message | None:
+        if not SessionData.__deleted_messages.get(guild_id, LifoQueue()).empty():
+            return SessionData.__deleted_messages[guild_id].get()
         return None
 
     @staticmethod
-    def get_recent_edited_message() -> dict | None:
-        if not SessionData.__edited_messages.empty():
-            return SessionData.__edited_messages.get()
+    def get_recent_edited_message(guild_id: int) -> dict | None:
+        if not SessionData.__edited_messages.get(guild_id, LifoQueue()).empty():
+            return SessionData.__edited_messages[guild_id].get()
         return None
 
     @staticmethod
     def record_deleted_message(message: discord.Message):
-        if not message.guild.id in SessionData.__deleted_messages:
+        if message.guild.id not in SessionData.__deleted_messages:
             SessionData.__deleted_messages[message.guild.id] = LifoQueue()
-        SessionData.__deleted_message[message.guild.id].put(message)
+        SessionData.__deleted_messages[message.guild.id].put(message)
 
     @staticmethod
     def record_edited_message(before: discord.Message, after: discord.Message):
-        if not before.guild.id in SessionData.__edited_messages:
+        if before.guild.id not in SessionData.__edited_messages:
             SessionData.__edited_messages[before.guild.id] = LifoQueue()
         SessionData.__edited_messages[before.guild.id].put(
             {"before": before, "after": after})
