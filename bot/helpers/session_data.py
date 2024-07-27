8f621d1db386255cd8e5fc5fe75834b34d0551ab
DragunDev <plarisanmarc@gmail.com>
2024-07-27 18:11:32 +0800

    Update session_data.py

--- bot/helpers/session_data.py
+++ bot/helpers/session_data.py
@@ -8,8 +8,18 @@ class SessionData:
 
     @staticmethod
     def get_recent_deleted_message() -> discord.Message:
-        return SessionData.__deleted_messages.get()
+        if not SessionData.__deleted_messages.empty():
+            return SessionData.__deleted_messages.get()
 
     @staticmethod
-    def get_recent_edited_message() -> tuple[discord.Message, discord.Message]:
-        return SessionData.__edited_messages.get()
+    def get_recent_edited_message() -> dict:
+        if not SessionData.__edited_messages.empty():
+            return SessionData.__edited_messages.get()
+
+    @staticmethod
+    def record_deleted_message(message: discord.Message):
+        SessionData.__deleted_message.put(message)
+
+    @staticmethod
+    def record_edited_message(before: discord.Message, after: discord.Message):
+        SessionData.__edited_messages.put({"before": before, "after": after})
