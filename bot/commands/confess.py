618c2ddd1c1b5353b5897bc764c683de55b7ffcd
DragunDev <plarisanmarc@gmail.com>
2024-07-29 06:36:35 +0800

    Update confess.py

--- bot/commands/confess.py
+++ bot/commands/confess.py
@@ -12,12 +12,12 @@ class Confess(commands.Cog):
                                   description="Setup a confessions channel. Type this command in the channel you want to designate the confessions channel to")
     async def setup(self, interaction: discord.Interaction):
         channel_id = interaction.channel_id
-        await interaction.response.send_message("")
+        await interaction.response.send_message("This command is still in development!")
 
     @discord.app_commands.command(name="confess", description="Submit a confession")
     @discord.app_commands.describe(message="The contents of your confession")
     async def confess(self, interaction: discord.Interaction, message: str):
-        await interaction.response.send_message("")
+        await interaction.response.send_message("This command is still under development!")
 
 
 async def setup(bot: commands.Bot):
