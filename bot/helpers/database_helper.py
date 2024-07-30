048532da037f96a148dbe522e9622a905a97adb0
DragunDev <plarisanmarc@gmail.com>
2024-07-30 21:44:23 +0800

    Update database_helper.py - Slight change to code format

--- bot/helpers/database_helper.py
+++ bot/helpers/database_helper.py
@@ -17,16 +17,16 @@ class DatabaseHelper:
         DatabaseHelper.__database_started = True
         firebase_admin.initialize_app(
             credentials.Certificate({
-                'type': os.environ.get('FIREBASE_TYPE'),
-                'project_id': os.environ.get('FIREBASE_PROJECT_ID'),
-                'private_key_id': os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-                'private_key': os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-                'client_email': os.environ.get('FIREBASE_CLIENT_EMAIL'),
-                'client_id': os.environ.get('FIREBASE_CLIENT_ID'),
-                'auth_uri': os.environ.get('FIREBASE_AUTH_URI'),
-                'token_uri': os.environ.get('FIREBASE_TOKEN_URI'),
-                'auth_provider_x509_cert_url': os.environ.get('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-                'client_x509_cert_url': os.environ.get('FIREBASE_CLIENT_X509_CERT_URL'),
+                "type": os.environ.get("FIREBASE_TYPE"),
+                "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+                "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+                "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+                "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+                "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+                "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+                "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+                "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+                "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
             }), {"databaseURL": os.environ.get("FIREBASE_DATABASE_URL")}
         )
         logging.info("Successfully connected to Realtime Firebase Database")
