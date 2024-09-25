52d71e85d1e80c2dbeee54e2fdc7c9955bce7ac9
DragunDev <plarisanmarc@gmail.com>
2024-09-25 16:06:52 +0800

    Update stats.py - Add work in progress message

--- bot/commands/stats.py
+++ bot/commands/stats.py
@@ -11,22 +11,29 @@ class Stats(commands.Cog):
 
     @discord.app_commands.command(name="stats", description="Check the user's stats")
     @discord.app_commands.describe(username="The Discord username of the user you want to check")
-    async def execute(self, interaction: discord.Interaction, username: str):
-        if not DatabaseHelper.is_username_exists():
-            await interaction.response.send_message(
-                (f'The user "{username}" is not recognized! The user has either not yet executed a'
-                 " DragunBot command or the user does not exist. Make sure you don't have any typos")
-            )
-            return
-        
-        data = DatabaseHelper.get_user(1)
-        embed = discord.Embed(title=f"{username}'s Stats", color=Utils.get_color("royal blue"))
-        embed.add_field(name="Commands Executed", value=f"`{data[Keys.COMMANDS_EXECUTED.value]}`", inline=False)
-        embed.add_field(name="Trivia Points", value=f"`{data[Keys.TRIVIA_POINTS.value]}`", inline=False)
-        embed.add_field(name="Times Counted", value=f"`{data[Keys.TIMES_COUNTED.value]}`", inline=False)
-        embed.set_footer(text="fuck")
-        
-        await interaction.response.send_message(embed=embed)
+    async def execute(self, interaction: discord.Interaction):
+        # add username parameter when you start developing
+        await interaction.response.send_message("Command is still in development!")
+
+        # if not DatabaseHelper.is_username_exists():
+        #     await interaction.response.send_message(
+        #         (f'The user "{username}" is not recognized! The user has either not yet executed a'
+        #          " DragunBot command or the user does not exist. Make sure you don't have any typos")
+        #     )
+        #     return
+
+        # data = DatabaseHelper.get_user(1)
+        # embed = discord.Embed(
+        #     title=f"{username}'s Stats", color=Utils.get_color("royal blue"))
+        # embed.add_field(name="Commands Executed",
+        #                 value=f"`{data[Keys.COMMANDS_EXECUTED.value]}`", inline=False)
+        # embed.add_field(name="Trivia Points",
+        #                 value=f"`{data[Keys.TRIVIA_POINTS.value]}`", inline=False)
+        # embed.add_field(name="Times Counted",
+        #                 value=f"`{data[Keys.TIMES_COUNTED.value]}`", inline=False)
+        # embed.set_footer(text="fuck")
+
+        # await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot):
